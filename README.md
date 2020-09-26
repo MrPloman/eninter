f4520e10677c5ea35937c16db7ebb23707e19560
MrPloman <pol.plana@slashmobility.com>
2020-09-26 15:28:49 +0200

    second

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# EninternForm
+# EninternTest
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.2.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to enintern-form!');
+    expect(page.getTitleText()).toEqual('Welcome to enintern-test!');
   });
 
   afterEach(async () => {
